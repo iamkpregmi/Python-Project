e2c25812ecbc08d12f979ce361ca3f6e80b46c55
Krishna Prasad Regmi <kpkrishna309@gmail.com>
2023-03-20 20:11:05 +0530

    Add files via upload

---
+++ Guessing Game.py
@@ -0,0 +1,19 @@
+import random
+
+chancess = 1
+number = random.randint(0,9)
+
+while (True):
+
+    guess  = int(input("Enter a number (0-9): "))
+
+    if (number == guess):
+        print("You've guessed the number correctly")
+        break
+    else:
+        print("You loss!")
+        chancess += 1
+
+    print("*****************")
+
+print("Chancess: ",chancess)
